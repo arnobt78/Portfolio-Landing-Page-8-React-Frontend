58e8d20482b5abb839d3a8465a1cabbdda7cdf3f
Arnob Mahmud <arnob_t78@yahoo.com>
2026-01-30 16:21:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Portfolio Landing Page 8 - React, Vite, Tailwind Frontend Project
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![React](https://img.shields.io/badge/React-18.3.1-blue)](https://react.dev/)
+[![Vite](https://img.shields.io/badge/Vite-6.2.0-green)](https://vite.dev/)
+[![Tailwind CSS](https://img.shields.io/badge/Tailwind%20CSS-4.1.17-blueviolet)](https://tailwindcss.com/)
---
+++ src/components/Target.jsx
@@ -0,0 +1,70 @@
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+const Target = (props) => {
+  const targetRef = useRef();
+
+  useGSAP(() => {
+    gsap.to(targetRef.current.position, {
+      y: targetRef.current.position.y + 0.5,
+      duration: 1.5,
+      repeat: -1,
+      yoyo: true,
+    });
+  });
+
+  return (
+    <group {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
+      {/* Stand base */}
+      <mesh position={[0, -1.5, 0]}>
+        <cylinderGeometry args={[0.5, 0.6, 0.2, 32]} />
+        <meshStandardMaterial color="#4a5568" metalness={0.6} roughness={0.4} />
+      </mesh>
+      
+      {/* Stand pole */}
+      <mesh position={[0, -0.5, 0]}>
+        <cylinderGeometry args={[0.08, 0.08, 2, 16]} />
+        <meshStandardMaterial color="#718096" metalness={0.7} roughness={0.3} />
+      </mesh>
+      
+      {/* Target board back */}
+      <mesh position={[0, 0.8, -0.05]}>
+        <cylinderGeometry args={[1, 1, 0.1, 32]} rotation={[Math.PI / 2, 0, 0]} />
+        <meshStandardMaterial color="#2d3748" />
+      </mesh>
+      
+      {/* Target rings - outermost (white) */}
+      <mesh position={[0, 0.8, 0]}>
+        <cylinderGeometry args={[1, 1, 0.05, 32]} />
+        <meshStandardMaterial color="#f7fafc" />
+      </mesh>
+      
+      {/* Target ring (red) */}
+      <mesh position={[0, 0.8, 0.01]}>
+        <cylinderGeometry args={[0.8, 0.8, 0.05, 32]} />
+        <meshStandardMaterial color="#e53e3e" />
+      </mesh>
+      
+      {/* Target ring (white) */}
+      <mesh position={[0, 0.8, 0.02]}>
+        <cylinderGeometry args={[0.6, 0.6, 0.05, 32]} />
+        <meshStandardMaterial color="#f7fafc" />
+      </mesh>
+      
+      {/* Target ring (red) */}
+      <mesh position={[0, 0.8, 0.03]}>
+        <cylinderGeometry args={[0.4, 0.4, 0.05, 32]} />
+        <meshStandardMaterial color="#e53e3e" />
+      </mesh>
+      
+      {/* Bullseye center (yellow/gold) */}
+      <mesh position={[0, 0.8, 0.04]}>
+        <cylinderGeometry args={[0.2, 0.2, 0.05, 32]} />
+        <meshStandardMaterial color="#ecc94b" />
+      </mesh>
+    </group>
+  );
+};
+
+export default Target;
